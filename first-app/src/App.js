98ff6dde19410cfa9967cb52a4c66bdc0d3fd814
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-02 05:26:20 +0200

    Handling events

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -3,9 +3,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo text="Learn React.js"/>
-      <Todo text="Learn Next.js"/>
-      <Todo text="Learn Node.js"/>
+      <Todo text="Learn React.js" />
+      <Todo text="Learn Next.js" />
+      <Todo text="Learn Node.js" />
     </div>
   );
 }
--- first-app/src/components/Todo.js
+++ first-app/src/components/Todo.js
@@ -1,9 +1,16 @@
 function Todo(props) {
+  function deleteHandler() {
+    console.log("Delete clicked");
+    console.log(props.text);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
