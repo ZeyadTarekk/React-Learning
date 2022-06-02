f464a9af0dcec6d11fc5615ff543505eacf4b790
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-02 05:09:06 +0200

    Creating Todo as a separte component

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,13 +1,11 @@
+import Todo from "./components/Todo";
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
   );
 }
---
+++ first-app/src/components/Todo.js
@@ -0,0 +1,12 @@
+function Todo() {
+  return (
+    <div className="card">
+      <h2>TITLE</h2>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
