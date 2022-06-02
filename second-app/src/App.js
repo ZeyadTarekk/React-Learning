2421e91926ec5b346fea033bf5ae3badca844a5b
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-02 23:29:08 +0200

    Adding navigation links

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -2,11 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 function App() {
   // localhost:3000/
   return (
     <div>
-      Hello
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
