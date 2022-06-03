7305164ee09f88275bf70979daebc106474b0cc7
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-03 06:03:25 +0200

    Prepare for the http request

--- second-app/src/components/meetups/NewMeetupForm.js
+++ second-app/src/components/meetups/NewMeetupForm.js
@@ -21,7 +21,7 @@ function NewMeetupForm(props) {
       address: enteredAddress,
       description: enteredDescription,
     };
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
   return (
     <Card>
--- second-app/src/pages/NewMeetup.js
+++ second-app/src/pages/NewMeetup.js
@@ -1,9 +1,11 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {}
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
