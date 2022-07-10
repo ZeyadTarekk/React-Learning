02956f0ce4fcdb7f5576cd4fb16d0d69cbced415
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-10 20:17:02 +0200

    using State

--- Udacity Nanodegree/React-Contacts/src/components/AllUsers.js
+++ Udacity Nanodegree/React-Contacts/src/components/AllUsers.js
@@ -1,31 +1,42 @@
-import User from "./User";
-const contacts = [
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    handle: "@tylermcginnis",
-    avatarURL: "http://localhost:5001/tyler.jpg",
-  },
-  {
-    id: "karen",
-    name: "Karen Isgrigg",
-    handle: "@karen_isgrigg",
-    avatarURL: "http://localhost:5001/karen.jpg",
-  },
-  {
-    id: "richard",
-    name: "Richard Kalehoff",
-    handle: "@richardkalehoff",
-    avatarURL: "http://localhost:5001/richard.jpg",
-  },
-];
+// import User from "./User";
+import { useState } from "react";
 
 function Users() {
+  const [contacts, setContacts] = useState([
+    {
+      id: "tyler",
+      name: "Tyler McGinnis",
+      handle: "@tylermcginnis",
+      avatarURL: "http://localhost:5001/tyler.jpg",
+    },
+    {
+      id: "karen",
+      name: "Karen Isgrigg",
+      handle: "@karen_isgrigg",
+      avatarURL: "http://localhost:5001/karen.jpg",
+    },
+    {
+      id: "richard",
+      name: "Richard Kalehoff",
+      handle: "@richardkalehoff",
+      avatarURL: "http://localhost:5001/richard.jpg",
+    },
+  ]);
   return (
     <ol className="contact-list">
       {contacts.map((user) => (
         <li className="contact-list-item " key={user.id}>
-          {<User user={user} />}
+          <div
+            className="contact-avatar"
+            style={{
+              backgroundImage: `url(${user.avatarURL})`,
+            }}
+          ></div>
+          <div className="contact-details">
+            <p>{user.name}</p>
+            <p>{user.handle}</p>
+          </div>
+          <button className="contact-remove">Remove</button>
         </li>
       ))}
     </ol>
