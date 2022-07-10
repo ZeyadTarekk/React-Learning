bd26908302ac004b639f8ef32e96c7ff23aa7cf9
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-10 22:06:10 +0200

    exercise 3

--- Udacity Nanodegree/React-Exercises/L3. State Management/3. Managing State/Starter/src/App.js
+++ Udacity Nanodegree/React-Exercises/L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,17 @@
 import corgi from "./corgi.jpg";
 import "./App.css";
-
+import { useState } from "react";
 const App = () => {
+  const increaseLikes = () => {
+    setLikes(likes + 1);
+  };
+  const [likes, setLikes] = useState(0);
   return (
     <div className="container">
       <h2>Like this photo!</h2>
+      <button onClick={increaseLikes}>Like</button>
       <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
+      <p>Amount of likes: {likes} </p>
     </div>
   );
 };
