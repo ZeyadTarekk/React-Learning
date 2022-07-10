139757fe2051d2aba81dabe29186eda8dc390dbc
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-10 23:39:22 +0200

    finish exercise 4

--- Udacity Nanodegree/React-Exercises/L3. State Management/4. Controlled Components I/Starter/src/App.js
+++ Udacity Nanodegree/React-Exercises/L3. State Management/4. Controlled Components I/Starter/src/App.js
@@ -1,7 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { useState } from "react";
 const App = () => {
+  const [enteredText, setEnteredText] = useState("");
+
+  const changeStateFunc = (e) => {
+    setEnteredText(e);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +15,17 @@ const App = () => {
         <h1 className="App-title">ReactND - Coding Practice</h1>
       </header>
       <div className="container">
-        <input type="text" placeholder="Say Something" />
+        <input
+          type="text"
+          placeholder="Say Something"
+          value={enteredText}
+          onChange={(e) => {
+            changeStateFunc(e.target.value);
+          }}
+        />
         <p className="echo">Echo:</p>
-        <p>This should mirror the text you typed into the input field.</p>
+        {enteredText !== "" ? enteredText : null}
+        {/* <p>This should mirror the text you typed into the input field.</p> */}
       </div>
     </div>
   );
