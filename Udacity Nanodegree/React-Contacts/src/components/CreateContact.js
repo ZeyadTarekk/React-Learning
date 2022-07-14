377569ec42762bd4cd0e7a9dd2a451b750d21017
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-14 14:54:29 +0200

    create contact form

--- Udacity Nanodegree/React-Contacts/src/components/CreateContact.js
+++ Udacity Nanodegree/React-Contacts/src/components/CreateContact.js
@@ -1,4 +1,24 @@
+import { Link } from "react-router-dom";
+import ImageInput from "./ImageInput";
 const CreateContact = () => {
-  return <div>CreateContact</div>;
+  return (
+    <div>
+      <Link to="/" className="close-create-contact">
+        Close
+      </Link>
+      <form className="create-contact-form">
+        <ImageInput
+          className="create-contact-avatar-input"
+          name="avatarURL"
+          maxHeight={64}
+        />
+        <div className="create-contact-details">
+          <input type="text" name="name" placeholder="Name" />
+          <input type="text" name="handle" placeholder="Handle" />
+          <button>Add Contact</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default CreateContact;
